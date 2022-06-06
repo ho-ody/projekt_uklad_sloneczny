351befd56dea58da3d24f25aae96dbc6df4b5360
ho-ody <r4f4l135@gmail.com>
2022-06-06 20:44:43 +0200

    VAO: dodano LinkAttrib - przekazywanie info do shadera vertex, większa ilość argumentów

--- G_L2/VAO.h
+++ G_L2/VAO.h
@@ -8,6 +8,7 @@ public:
 	VAO();
 
 	void LinkVBO(VBO& VBO, GLuint positionLocation, GLuint colorLocation);
+	void LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset);
 	void Bind();
 	void Unbind();
 	void Delete();
